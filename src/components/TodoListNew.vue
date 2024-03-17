ae742aa46abc23c8d77f410f024f2bbf87d6b50b
마성일 <masungil70.gmail.com>
2024-03-17 20:54:24 +0900

    TodoListContainer를 TodoListNew와 TodoListMain으로 분리함

---
+++ src/components/TodoListNew.vue
@@ -0,0 +1,30 @@
+/*
+TodoList에 등록할 항목 UI
+ */
+<template>
+  <section>
+    <div>
+      <div class="col border border-primary rounded">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="여기에 할일을 적으세요"
+        />
+        <div class="row">
+          <div class="col-6"><input type="date" class="float-start" /></div>
+          <div class="col-6">
+            <button type="button" class="btn btn-primary btn-sm float-end">
+              작업추가
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
