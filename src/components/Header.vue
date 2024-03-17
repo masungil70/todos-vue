36da13b922c6f0d02031b50d8dc0230729d39f69
마성일 <masungil70.gmail.com>
2024-03-17 20:28:41 +0900

    1. 기본  layout 화면을 Header와 TodoListContainer로 분리하여 UI를 작성함

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+/* header UI를 구성함 */
+<template>
+  <header>
+    <hgroup>
+      <h1>나의 할일</h1>
+      <em>2024-03-17</em>
+    </hgroup>
+  </header>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
