fab6c3592119bda7cf86e14da3d77fbdcc16f795
마성일 <masungil70.gmail.com>
2024-03-17 23:21:37 +0900

    vue3에 부트스트랩5가 적용되지 않아 downlist가 출력되지 않아 기능 수정함

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import BootstrapVue3 from 'bootstrap-vue-3'
 
 //부트스트랩 css 적용 
 import 'bootstrap/dist/css/bootstrap.css'
-
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 createApp(App)
+    .use(BootstrapVue3)    
     .mount('#app')
+``
