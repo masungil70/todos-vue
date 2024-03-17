c02045ff75f314ef03cc35b0b7173b80de09b2f3
마성일 <masungil70.gmail.com>
2024-03-17 20:13:06 +0900

    1. 부트스트립 css 적용 2. 초기 화면 layout 구성

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+//부트스트랩 css 적용 
+import 'bootstrap/dist/css/bootstrap.css'
+
+createApp(App)
+    .mount('#app')
