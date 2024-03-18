24c8d67aeb303a0e4ec282656c10a6752a2498e8
masungil <masungil70@gmail.com>
2024-03-18 16:41:06 +0900

    TodoListContrainer에서 배열을 관리할 수 있게 기능 수정함. 배열에 객체를 추가할 때 사용될 id을 함수로 분리함 테

--- src/components/TodoListContainer.vue
+++ src/components/TodoListContainer.vue
@@ -1,11 +1,52 @@
 <template>
   <TodoListNew />
-  <TodoListMain />
+  <TodoListMain :todoList="todoList" />
 </template>
 
 <script setup>
+import { ref } from "vue";
 import TodoListNew from "./TodoListNew.vue";
 import TodoListMain from "./TodoListMain.vue";
+
+//전역 할일 아이디 변수선언(101값은 내가 임의로 선언 초기값)
+const todoId = ref(101);
+//다음 할일 아아디를 얻는 함수 
+const nextTodoId = () => {
+  return todoId.value++;
+}
+
+//전체 작업들
+const todoList = ref([]);
+
+//할일을 등록하는 함수
+const addTodoItem = (title, date) => {
+  todoList.value.push({
+      id: nextTodoId(),
+      title: title,
+      date: date,
+      completed: false,
+  })
+}
+
+//할일 ID을 찾아 삭제한다
+const removeTodoItem = (id) => {
+  todoList.value = todoList.value.filter(todoItem => todoItem.id != id);
+}
+
+//테스트 코드 : 나중에 삭제할 예정 
+//sample 데이터 추가
+addTodoItem("오늘 할일 제목1", "2024-03-17");
+addTodoItem("오늘 할일 제목2", "2024-03-17");
+addTodoItem("오늘 할일 제목3", "2024-03-17");
+addTodoItem("오늘 할일 제목4", "2024-03-17");
+addTodoItem("오늘 할일 제목11", "2024-03-17");
+addTodoItem("오늘 할일 제목12", "2024-03-17");
+addTodoItem("오늘 할일 제목13", "2024-03-17");
+
+//테스트 코드 : 나중에 삭제할 예정 
+//sample 데이터 103삭제 후 화면에 103이 나오지 않으면 됨 
+removeTodoItem(103);
+
 </script>
 
 <style  scoped>
--- src/components/TodoListMain.vue
+++ src/components/TodoListMain.vue
@@ -15,21 +15,14 @@ import TodoListMenu from "./TodoListMenu.vue";
 import TodoList from "./TodoList.vue";
 import { ref } from "vue";
 
-//전체 작업들
-const todoList = ref([
-  { id: 1, date: "2024-03-17", title: "오늘 할일 제목1", completed: true },
-  { id: 2, date: "2024-03-17", title: "오늘 할일 제목2", completed: true },
-  { id: 3, date: "2024-03-17", title: "오늘 할일 제목3", completed: true },
-  { id: 4, date: "2024-03-17", title: "오늘 할일 제목4", completed: true },
-  { id: 11, date: "2024-03-17", title: "오늘 할일 제목11", completed: false },
-  { id: 12, date: "2024-03-17", title: "오늘 할일 제목12", completed: false },
-  { id: 13, date: "2024-03-17", title: "오늘 할일 제목13", completed: false },
-]);
+const props = defineProps(["todoList"]);
+const todoList = ref(props.todoList);
 
 //처리하지 못한 작업들
 const pendingList = () => {
   return todoList.value.filter((item) => item.completed == false);
 };
+
 </script>
 
 <style lang="scss" scoped>
